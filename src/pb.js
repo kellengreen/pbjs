215ae9ca0eab5fa46da269ed12e22d2ffa800521
Kellen Green <me@kellengreen.com>
2016-03-13 20:07:24 +0000

    wip

--- src/pb.js
+++ src/pb.js
@@ -1,104 +1,140 @@
 'use strict';
 (function() {
 
-    /**
-     * Symbols
-     */
-
-    const listeners = Symbol('listeners');
-
-    /**
-     * PbProxy
-     */
-
-    class PbObj {
-
-        static proxy(obj) {
+    class PbManager {
+        /**
+         * PbManager
+         */
+        constructor() {
             /**
              *
              */
-            Object.defineProperty(obj, listeners, {
-                value: {}
+            this.drawScheduled = new Set();
+            this.drawListeners = {};
+            this.liveListeners = {};
+
+            this.path = Symbol('path');
+            this.obj = this.proxy({}, null);
+
+            // now, live,
+            // raf, draw, delay
+        }
+
+        proxy(obj, parent) {
+            /**
+             * Create a proxy
+             */
+
+            Object.defineProperty(obj, this.path, {
+                value: parent ? parent[this.path] : ''
             });
 
-            let handler = {
-                set: PbObj.set
+            if (typeof val === 'object') {
+                val = this.proxy(val);
+            }
+
+            const handler = {
+                set: this.setHandler
             };
 
             return new Proxy(obj, handler);
         }
 
-        static addListener(obj, key, callback) {
+        setHandler(obj, key, val) {
             /**
-             *
-             */
-            let symbol = Symbol('listener');
-
-            if (obj[listeners][key] === undefined) {
-                obj[listeners][key] = {};
-            }
-
-            obj[listeners][key][symbol] = callback;
-            return symbol;
-        }
-
-        static removeListener(obj, key, symbol) {
-            /**
-             *
-             */
-            if (obj[listeners][key] !== undefined) {
-                delete obj[listeners][key][symbol];
-            }
-        }
-
-        static set(obj, key, val) {
-            /**
-             *
+             * Trap function for set property
              */
 
-            // convert to proxy if necessary
+            //
             if (typeof val === 'object') {
-                val = PbObj.proxy(val);
+                val = new String(val);
             }
 
-            // call listeners
-            if (obj[listeners][key]) {
-                for (let symbol of Object.getOwnPropertySymbols(obj[listeners][key])) {
-                    obj[listeners][key][symbol](val);
-                }
-            }
+            // listeners
+            this.setOrExecListeners(obj, this.liveListeners);
+            this.setOrExecListeners(obj, this.drawListeners);
 
+            val = this.proxy(val);
             return obj[key] = val;
         }
 
-        static getByPath(path) {
+        addLiveListener(path, fn) {
+            if (this.liveListeners[path]) {
+                this.liveListeners[path].add(fn);
+            } else {
+                this.liveListeners[path] = new Set([fn]);
+            }
+        }
+
+        addDrawListener(path, fn) {
+            if (this.drawListeners[path]) {
+                this.drawListeners[path].add(fn);
+            } else {
+                this.drawListeners[path] = new Set([fn]);
+            }
+        }
+
+        removeLiveListener(path, fn) {
+            if (this.liveListeners[path]) {
+                this.liveListeners[path].remove(fn);
+            }
+        }
+
+        removeDrawListener(path, fn) {
+            if (this.drawListeners[path]) {
+                this.drawListeners[path].remove(fn);
+            }
+        }
+
+        setOrExecListeners(obj, key) {
+            /**
+             *
+             */
+            if (obj[key]) {
+                obj[key].forEach(function(callback) {
+                    callback(obj, key);
+                });
+            } else {
+                obj[key] = new Set();
+            }
+        }
+
+        drawCallback() {
+            /**
+             *
+             */
+            this.rafScheduled = false;
+            this.rafCallbacks.forEach(function(callback) {
+                callback();
+            }.bind(this));
+        }
+
+        getObjKeyPair(path) {
             /**
              *
              */
             const keys = path.split('.');
-            let val = pb;
+            let obj = this.obj;
+            let key;
             for (let i = 0; i < keys.length; i++) {
-                let key = keys[i];
-                val = val[key];
-                if (typeof val !== 'object' && i !== keys.length - 1) {
-                    return;
+                key = keys[i];
+                if (typeof obj[key] !== 'object' && i !== keys.length - 1) {
+                    throw `Path does not exist: ${path}`;
                 }
             }
-            return val;
+            return {obj, key};
         }
     }
 
-    /**
-     * PbElement
-     */
-
     class PbElement extends HTMLElement {
-
-        static init(tagName) {
+    /**
+     * PbManager
+     */
+        static register(tagName) {
             /**
              *
              */
-            this.Element = document.registerElement(tagName, {
+            document.registerElement(tagName, {
                 prototype: this.prototype
             });
         }
@@ -107,6 +143,10 @@
             /**
              *
              */
+            this.attrs.forEach(function(name) {
+                const val = this.getAttribute(name);
+                this.attributeChangedCallback(name, undefined, val);
+            }.bind(this));
         }
 
         attchedCallback() {
@@ -125,9 +165,12 @@
             /**
              *
              */
-            let fn = attrName + 'Changed';
-            if (typeof this[fn] === 'function') {
-                this[fn](newVal);
+            const fnName = attrName.replace(/-([a-z])/g, function (m, p1) {
+                return p1.toUpperCase();
+            }) + 'Changed';
+
+            if (this[fnName]) {
+                this[fnName](newVal, oldVal);
             }
         }
     }
@@ -137,46 +180,63 @@
      */
 
     class PbBindElement extends PbElement {
-        /**
-         *
-         */
         createdCallback() {
             /**
              *
              */
-            console.log('created');
-            this.idChanged(this.getAttribute('pb-id'));
+            this.attrs = new Set(['bind-to']);
+            super.createdCallback();
         }
 
         detachedCallback() {
             /**
              *
              */
-            this.value[PbObj.removeListener](this.listener);
-
+            // this.value[pbManager.removeListener](this.listener);
         }
 
-        idChanged(value) {
+        bindToChanged(newVal, oldVal) {
             /**
              *
              */
-            //this.value = PbObj.getByPath(value);
-            //const fn = this.idChanged.bind(this);
-            //this.listener = this.value[PbObj.addListener](fn);
-            //this.textContent = this.value;
+            if (oldVal) {
+                pbManager.removeListener(oldVal);
+            }
+
+            //this.callback = function(val) {
+            //    window.requestAnimationFrame(function() {
+            //        const {obj, key} = PbObj.getObjKeyPair(newVal);
+            //        this.textContent = val;
+            //    }.bind(this));
+            //};
+
+            this.callback = function(obj, key) {
+                this.render(obj, key);
+            }.bind(this);
+
+            pbManager.addListener(newVal, this.callback);
+            const {obj, key} = pbManager.getObjKeyPair(newVal);
+            this.render(obj, key);
+        }
+
+        render(obj, key) {
+            this.textContent = obj[key];
         }
     }
-    PbBindElement.init('pb-bind');
 
     /**
      * Globals
      */
 
-    window.Pb = {
-        addListener: PbObj.addListener,
-        removeListener: PbObj.removeListener
-    };
+    const pbManager = new PbManager();
 
-    window.pb = PbObj.proxy({});
+    window.Pb = pbManager;
+    window.pb = pbManager.obj;
+
+    /**
+     * Register
+     */
+
+    PbBindElement.register('pb-bind');
 
 })();
