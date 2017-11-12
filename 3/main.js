6b76a37adb0bf94a8042bc1513a9e12293a9183c
Kellen Green <me@kellengreen.com>
2017-11-12 19:58:34 +0000

    wip

--- 3/main.js
+++ 3/main.js
@@ -1,14 +1,17 @@
-import Element from './pb/Element.js';
-import ElementManager from './pb/ElementManager.js';
-// import storage from './pb/storage.js';
-import {x} from './pb/storage.js';
+// import Element from './pb/Element.js';
+// import ElementManager from './pb/ElementManager.js';
+// import {x} from './pb/storage.js';
 
-class MyManager extends ElementManager {
+// class MyManager extends ElementManager {
     
-}
-
-const el = new ElementManager();
-
-window.s = storage;
+    // }
+    
+    // const el = new ElementManager();
+import Storage from './pb/Storage.js';
+    
+window.s = Storage.object({});
+Storage.listen(s, 'foo', (val) => {
+    console.log(`set: ${val}`);
+})
 
 // customElements.define('test-one', MyManager);
--- 3/pb/storage.js
+++ 3/pb/storage.js
@@ -1,40 +1,96 @@
-const allowedObjects = [
-    Array,
-    Map,
-    Set
-]
+import symbol from './symbol.js';
 
+/**
+ * 
+ */
 class Storage {
     /**
-     * 
+     * @param {object}
+     * @returns {proxy}
      */
-    static proxy(target) {
-        return new Proxy(target, this)
+    static object(object) {
+        object[symbol] = {};
+        return new Proxy(object, {
+            set: this.setHandler.bind(this),
+            deleteProperty: this.deleteHandler.bind(this),
+        });
     }
 
     /**
-     * 
+     * @param {proxy} proxy
+     * @param {string} property 
+     * @param {function} callback 
+     * @returns {undefined}
      */
-    static get(target, property, receiver) {
-        if (target[property] === undefined) {
-            target[property] = this.proxy({});
+    static listen(proxy, property, callback) {
+        let set = proxy[symbol][property];
+        if (set === undefined) {
+            set = new Set();
+            proxy[symbol][property] = set;
         }
-        return target[property];
+        set.add(callback);
     }
-
+    
     /**
-     * 
+     * @param {proxy} object
+     * @param {string} property 
+     * @param {function} callback 
+     * @returns {undefined}
      */
-    static set(target, property, value, receiver) {
-
+    static ignore(proxy, property, callback) {
+        let set = proxy[symbol][property];
+        if (set !== undefined) {
+            set.delete(callback);
+        }
+    }
+    
+    /**
+     * Handler for set requests.
+     * @param {*} target - The object we're looking to modify.
+     * @param {string} property - Name of the property to be set.
+     * @param {*} value - Value to be set in the object.
+     * @param {*} receiver - The proxy object.
+     * @returns {boolean} - 
+     */
+    static setHandler(target, property, value, receiver) {
+        console.dir(this);
+        // Create a new proxy if value is not a primitive.
         if (value instanceof Object) {
-            value  = this.proxy(value);
+            value  = this.object(value);
         }
+        target[property] = value;
+        this.invokeCallbacks(target, property);
+        return true;
+    }
+    
+    
+    /**
+     * Handler for set requests.
+     * @param {*} target - The object we're looking to modify.
+     * @param {string} property - Name of the property to be set.
+     * @returns {boolean} - 
+     */
+    static deleteHandler(target, property) {
+        delete target[property];
+        this.invokeCallbacks(target, property);
+        return true;
+    }
 
-        return target[property] = value;
+    /**
+     * 
+     * @param {object} target 
+     * @param {string} property 
+     * @returns {undefined}
+     */
+    static invokeCallbacks(target, property) {
+        const value = target[property];
+        const callbacks = target[symbol][property];
+        if (callbacks !== undefined) {
+            for (const callback of callbacks.values()) {
+                callback(value);
+            }
+        }
     }
 }
 
-const storage = Storage.proxy({});
-
-export default storage;
+export default Storage;
